36753f41c1eb2997fcb13c71c7e92375fcd03e8e
Viktor <vila@vilax.(none)>
2012-07-08 22:34:34 +0200

    removed cedet

--- vila-input.el
+++ vila-input.el
@@ -26,24 +26,3 @@
 
 ;; Toggle fullscreen emacs
 (global-set-key (kbd "<f12>") 'toggle-fullscreen)
-
-(defun vila-cedet-hook ()
-  (local-set-key [(control return)] 'semantic-ia-complete-symbol-menu)
-  (local-set-key "\C-c?" 'semantic-ia-complete-symbol)
-  (local-set-key (kbd "<C-SPC>") 'semantic-complete-analyze-inline)
-  ;;(local-set-key "\C-c=" 'semantic-decoration-include-visit)
-
-  (local-set-key "\C-cj" 'semantic-ia-fast-jump)
-  ;;  (local-set-key "\C-cq" 'semantic-ia-show-doc)
-  ;;  (local-set-key "\C-cs" 'semantic-ia-show-summary)
-  (local-set-key "\C-cp" 'semantic-analyze-proto-impl-toggle)
-
-  ;;(local-set-key "." 'semantic-complete-self-insert)
-  ;;(local-set-key ">" 'semantic-complete-self-insert)
-  (local-set-key "\C-ct" 'eassist-switch-h-cpp)
-  (local-set-key "\C-xt" 'eassist-switch-h-cpp)
-  (local-set-key "\C-ce" 'eassist-list-methods)
-  (local-set-key "\C-c\C-r" 'semantic-symref))
-
-(when 'vila-cedet-loaded
-  (add-hook 'c-mode-common-hook 'vila-cedet-hook))
